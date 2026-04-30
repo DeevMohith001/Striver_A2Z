07fc139a218d7f262b12819c4e2ad83b97d40fcd
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-04-30 22:28:39 +0530

    Updated Pattern3.java

--- src/Patterns/Pattern3.java
+++ src/Patterns/Pattern3.java
@@ -2,10 +2,11 @@ package Patterns;
 
 public class Pattern3 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j < 5-j; j++) {
-
+        for (int i = 1; i <= 5; i++) {        // rows
+            for (int j = 1; j <= i; j++) {    // numbers
+                System.out.print(j);
             }
+            System.out.println();
         }
     }
 }
