2849670b9ab8d1a416afff36e025c6522e283af0
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-01 22:35:38 +0530

    Added Pattern6.java

---
+++ src/Patterns/Pattern7.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+import java.util.Scanner;
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // move to next line
+            System.out.println();
+        }
+    }
+}
