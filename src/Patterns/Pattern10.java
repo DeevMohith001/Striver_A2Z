4ac263f2419eb06412ed0d328cf5dd2af16141e6
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-02 21:17:36 +0530

    Added Pattern10.java

---
+++ src/Patterns/Pattern10.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
