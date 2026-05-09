3f47fab8a7e4cd6768e0faafb3813344b63b8d5b
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-10 01:56:57 +0530

    Added Pattern17.java

---
+++ src/Patterns/Pattern17.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+
+            // increasing
+            for (int j = 0; j < i; j++) {
+                System.out.print((char)('A'+j));
+            }
+
+            // Decreasing
+            for (int j = i-2; j >= 0; j--) {
+                System.out.print((char)('A'+j));
+            }
+            System.out.println();
+        }
+    }
+}
