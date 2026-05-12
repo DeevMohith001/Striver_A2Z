bac5b86ad0314eeaddd3f9ca62bcb1bdb11ea401
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-12 23:49:28 +0530

    Added count all digits of a number

---
+++ src/BasicMaths/CountDigits.java
@@ -0,0 +1,22 @@
+package BasicMaths;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int result = countDigits(123098);
+        System.out.println(result);
+
+    }
+
+    static int countDigits(int n){
+        if (n==0){
+            return 1;
+        }
+
+        int count = 0;
+        while (n!=0){
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+}
