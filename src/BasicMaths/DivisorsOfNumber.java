a7dd34ccd532c61ab31d1f03e4165b5592140604
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-15 22:36:04 +0530

    Added file which returns the divisors of a number

---
+++ src/BasicMaths/DivisorsOfNumber.java
@@ -0,0 +1,32 @@
+package BasicMaths;
+
+import java.util.Arrays;
+
+public class DivisorsOfNumber {
+    public static void main(String[] args) {
+        int[] r = divisors(18);
+        System.out.println(Arrays.toString(r));
+    }
+
+    static int[] divisors(int n){
+        int count=0;
+        for (int i = 1; i <= n; i++) {
+            if (n%i==0){
+                count++;
+            }
+        }
+
+        int[] arr = new int[count];
+
+        int index = 0;
+
+        // Second pass: store divisors
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                arr[index] = i;
+                index++;
+            }
+        }
+        return arr;
+    }
+}
