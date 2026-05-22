0282afcd8e929ec0781e73d3a1b86c9dc195c2c2
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-23 05:24:55 +0530

    Added file which returns second largest element in an array

---
+++ src/Arrays/Easy/SecondLargestElement.java
@@ -0,0 +1,25 @@
+package Arrays.Easy;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] arr = {34, 57, 126, -90, 24, 68, 65};
+        int r = secondLargestElement(arr);
+        System.out.println(r);
+    }
+
+    static int secondLargestElement(int[] nums){
+        int largest = nums[0];
+        int secLargest = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>largest){
+                secLargest = largest;
+                largest = nums[i];
+            }
+            else if (nums[i]>secLargest && nums[i]<largest){
+                secLargest = nums[i];
+            }
+        }
+        return secLargest;
+    }
+}
