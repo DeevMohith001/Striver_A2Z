9ab73d0f669c2cbc7afc5b8730a95dd6c90d669b
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-07-07 22:19:02 +0530

    compute lower bound

---
+++ src/BinarySearch/q02_lowerBound.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class q02_lowerBound {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 9, 11, 12, 14, 20, 36, 48};
+        int target = 21;
+        int result = lowerBound(arr, target);
+        System.out.println(result);
+    }
+    static int lowerBound(int[] nums, int x){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = nums.length; // Default if no element >= x exists
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= x) {
+                ans = mid;        // Potential answer
+                high = mid - 1;   // Look for an earlier occurrence
+            } else {
+                low = mid + 1;    // Search in the right half
+            }
+        }
+
+        return ans;
+    }
+}
