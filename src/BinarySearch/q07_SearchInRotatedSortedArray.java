78f24409a553a67c489ababc339428a2879b270c
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-15 06:49:24 +0530

    SearchInRotatedSortedArray

---
+++ src/BinarySearch/q07_SearchInRotatedSortedArray.java
@@ -0,0 +1,42 @@
+package BinarySearch;
+
+public class q07_SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+    public static int search(int[] nums, int target) {
+        int s =0;
+        int e = nums.length-1;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m]==target){
+                return m;
+            } if (nums[s] <= nums[m]) {
+
+                // Target lies in the sorted left half
+                if (nums[s] <= target && target < nums[m]) {
+                    e = m - 1;
+                }
+                // Target lies in the right half
+                else {
+                    s = m + 1;
+                }
+
+            }
+            // Right half is sorted
+            else {
+
+                // Target lies in the sorted right half
+                if (nums[m] < target && target <= nums[e]) {
+                    s = m + 1;
+                }
+                // Target lies in the left half
+                else {
+                    e = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
