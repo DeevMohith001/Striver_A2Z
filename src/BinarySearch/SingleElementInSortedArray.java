985f0b8e30ac214c8bce8ef9f7d139387a3fbf89
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-23 22:50:19 +0530

    SingleElementInSortedArray

---
+++ src/BinarySearch/SingleElementInSortedArray.java
@@ -0,0 +1,27 @@
+package BinarySearch;
+
+public class SingleElementInSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+        int r = search(nums);
+        System.out.println(r);
+    }
+
+    public static int search(int[] nums){
+        int s = 0;
+        int e = nums.length-1;
+        while (s<e){
+            int m= s+(e-s)/2;
+            if (m%2==1){
+                m--;
+            }
+            if (nums[m] == nums[m+1]){
+                s = m+2;
+            }
+            else {
+                e = m;
+            }
+        }
+        return nums[s];
+    }
+}
