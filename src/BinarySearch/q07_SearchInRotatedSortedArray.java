485a831e478a156972431e38b475b6edb9871266
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-23 22:13:29 +0530

    SearchInRotatedSortedArray-2

--- src/BinarySearch/q07_SearchInRotatedSortedArray.java
+++ src/BinarySearch/q07_SearchInRotatedSortedArray.java
@@ -2,7 +2,10 @@ package BinarySearch;
 
 public class q07_SearchInRotatedSortedArray {
     public static void main(String[] args) {
-
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        int r = search(nums, target);
+        System.out.println(r);
     }
 
     public static int search(int[] nums, int target) {
